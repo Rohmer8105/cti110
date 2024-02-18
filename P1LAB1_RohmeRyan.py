5ef25283cdb66a66156edafe7829010944dcc796
Rohmer8105 <160424447+Rohmer8105@users.noreply.github.com>
2024-02-18 16:04:01 -0500

    Add files via upload
    First Submission

---
+++ P1LAB1_RohmeRyan.py
@@ -0,0 +1,12 @@
+print('# Ryan')
+print('# February 18, 2024')
+print('# P1LAB1')
+print("# Use Python's input and print functions to complete the program")
+
+# Prompt the user to enter their first name
+first_name = input("Enter your first name: ")
+
+# Print a welcome message using the input name
+print("Hey " + first_name)
+print("Welcome to zyBooks!")
+
