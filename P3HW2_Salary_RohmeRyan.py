4160a13710e0e8fd0de65ee34b1f623ceba5dd54
Rohmer8105 <160424447+Rohmer8105@users.noreply.github.com>
2024-04-06 18:05:38 -0400

    Add files via upload

---
+++ P3HW2_Salary_RohmeRyan.py
@@ -0,0 +1,23 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:\Users\ryanr\OneDrive\Desktop\P3HW2_Salary_RohmeRyan.py ======
+Enter employee's name: kfjnewknf
+Enter number of hours worked: 1000
+Enter employee's pay rate: 20
+--------------------------------------------------------------------------------
+Employee name: kfjnewknf      
+Hours worked: 1000.00         Pay Rate: 20.00          
+Overtime Hours: 960.00          Overtime Pay: 28800.00       
+Regular Hours Pay: 800.00          Gross Pay: 29600.00       
+--------------------------------------------------------------------------------
+>>> 
+====== RESTART: C:\Users\ryanr\OneDrive\Desktop\P3HW2_Salary_RohmeRyan.py ======
+Enter employee's name: wkejnfewjknf
+Enter number of hours worked: 20
+Enter employee's pay rate: 700
+-----------------------------------------------
+Employee name: wkejnfewjknf
+Hours worked	Pay Rate	Overtime	Overtime Pay	Reghour Pay	Gross Pay
+-------------------------------------------------------------------------------------------------
+20.00		700.00		0.00		0.00		14000.00		14000.00
